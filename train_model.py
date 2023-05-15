a696eae152e67b135c3a67f49ecad379b3356da0
samuelwheeler <91495954+samuelwheeler@users.noreply.github.com>
2023-05-15 09:36:08 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -40,7 +40,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 # define model:
-model = ViT_model.ViT(image_size = image_size, patch_size = patch_size, num_classes = 10, dim = dim, depth = numblocks, mlp_dim = dim, attention_type = 'standard', 
+model = ViT_model.ViT(image_size = image_size, patch_size = patch_size, num_classes = 10, dim = dim, depth = numblocks, mlp_dim = dim, attention_type = 'transposed', 
             heads = heads, dropout = 0., emb_dropout = 0., fixed_size = False)
 starting_epoch = 0
 
