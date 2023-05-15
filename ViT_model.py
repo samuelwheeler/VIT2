ab789b480bab809a5d611ba0fb6a5b8b04e308f4
samuelwheeler <91495954+samuelwheeler@users.noreply.github.com>
2023-05-15 08:13:02 -0500

    Update ViT_model.py

--- ViT_model.py
+++ ViT_model.py
@@ -633,9 +633,12 @@ class ViT(nn.Module):
         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
         self.dropout = nn.Dropout(emb_dropout)
-
-        self.transformer = Transformer(attention_type = attention_type, dim = dim, depth = depth-1, heads = heads, dim_head = dim_head, mlp_dim = mlp_dim,
-                                       dropout = dropout, num_patches = num_patches, fixed_size = self.fixed_size)
+        if fixed_size:
+            self.transformer = Transformer(attention_type = attention_type, dim = dim, depth = depth-1, heads = heads, dim_head = dim_head, mlp_dim = mlp_dim,
+                                        dropout = dropout, num_patches = num_patches, fixed_size = self.fixed_size)
+        else:
+            self.transformer = Transformer(attention_type = attention_type, dim = dim, depth = depth, heads = heads, dim_head = dim_head, mlp_dim = mlp_dim,
+                                        dropout = dropout, num_patches = num_patches, fixed_size = self.fixed_size)
 
         self.pool = pool
         self.to_latent = nn.Identity()
@@ -645,7 +648,7 @@ class ViT(nn.Module):
             nn.Linear(dim, num_classes)
         )
         if self.fixed_size:
-            self.first_transformer = Transformer(attention_type = attention_type, dim = dim, depth = 1, heads = heads, dim_head = dim_head, mlp_dim = mlp_dim,
+            self.first_transformer = Transformer(attention_type = 'standard', dim = dim, depth = 1, heads = heads, dim_head = dim_head, mlp_dim = mlp_dim,
                                        dropout = dropout, num_patches = num_patches, fixed_size = False)
             #self.fix_length = nn.Linear(dim, 64, bias = False)
             self.fl_net = nn.Sequential(
