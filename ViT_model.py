ec464286d303189419cdac012f2930468d0852b3
samuelwheeler <91495954+samuelwheeler@users.noreply.github.com>
2023-07-05 05:35:15 -0500

    Update ViT_model.py

--- ViT_model.py
+++ ViT_model.py
@@ -65,6 +65,42 @@ class Attention(nn.Module):
         out = torch.matmul(attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
         return self.to_out(out)
+        
+class All_Same_Shuffle(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+        perm1 = list(range(dim)) + random.sample(range(dim, l*dim), (l-1)*dim)
+
+    def forward(self, x):
+        flat_x = x.flatten(start_dim=1)
+
+        flat_x_shuffled = flat_x[:, self.permq]
+
+        shuffled_x = flat_x_shuffled.view(x.size())
+        
+        qkv = self.to_qkv(shuffled_x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
 
 class TransposedAttention(nn.Module):
     def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
@@ -641,6 +677,12 @@ class Transformer(nn.Module):
             for _ in range(depth):
                 self.layers.append(nn.ModuleList([
                     PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                    PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+                ]))
+        elif attention_type == 'all_same_shuffle':
+            for _ in range(depth):
+                self.layers.append(nn.ModuleLust([
+                    PreNorm(dim, All_Same_Shuffle(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
                     PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
                 ]))
 
